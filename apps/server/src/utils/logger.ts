e6830edcf65aed868bb0ca5bde516f260368ecd5
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2025-06-18 13:49:50 +0500

    server logger for production

--- apps/server/src/utils/logger.ts
+++ apps/server/src/utils/logger.ts
@@ -1,15 +1,19 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import {fileURLToPath} from 'node:url';
-import { LOG_PATH_FROM_UTILS_LOGGER } from '../config/dotenv.config.js';
+import { LOG_PATH_FROM_UTILS_LOGGER, NODE_ENV } from '../config/dotenv.config.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const logger = {
   log: (message: string) => {
-    const logPath = path.join(__dirname, '../../logs/app.log');
-    fs.appendFileSync(logPath, `${new Date().toISOString()} ${message}\n`);
+    if (NODE_ENV === 'development') {
+      const logPath = path.join(__dirname, LOG_PATH_FROM_UTILS_LOGGER);
+      fs.appendFileSync(logPath, `${new Date().toISOString()} ${message}\n`);
+    } else {
+      console.log(`${new Date().toISOString()} ${message}\n`);
+    }
   } 
 };
 
