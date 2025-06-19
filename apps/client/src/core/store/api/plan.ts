2700ed3930dd5e56cc1b9cafdb0e849121736da0
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2025-06-19 13:42:26 +0500

    client add cache

--- apps/client/src/core/store/api/plan.ts
+++ apps/client/src/core/store/api/plan.ts
@@ -5,7 +5,7 @@ const plansListApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getPlans: builder.query<{ success: boolean, data: IPlan[] }, void>({
       query: () => '/api/v1/plans',
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 60
     }),
   }),
   overrideExisting: false,
@@ -15,7 +15,7 @@ const addonnsListApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getAddonns: builder.query<{ success: boolean, data: IAddon[] }, void>({
       query: () => '/api/v1/addons',
-      keepUnusedDataFor: 0
+      keepUnusedDataFor: 60
     }),
   }),
   overrideExisting: false,
